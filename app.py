de164d8e6a678d6b5ead6cd8fb973715d3bb6280
Maha <123593287+MahaFa1@users.noreply.github.com>
2025-04-20 15:25:15 +0300

    Switch to env variables

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import requests
 from flask import Flask, request, jsonify, send_from_directory
 import mysql.connector
 import openai
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
+elevenlabs_api_key = os.getenv("ELEVENLABS_API_KEY")
 
 app = Flask(__name__, static_folder='public', static_url_path='')
 
 # إعداد OpenAI API – استبدل المفتاح بمفتاحك الخاص
 
-import config
+#import config
 openai.api_key = config.OPENAI_API_KEY
 ELEVENLABS_API_KEY = config.ELEVENLABS_API_KEY
 # إعدادات قاعدة البيانات MySQL
